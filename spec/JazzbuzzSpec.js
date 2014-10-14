bd87cd1c5668de52f13798d28445063152459248
RachelNolan <mail@rachelnolan.com>
2014-10-14 14:21:55 +0100

    Knows when number is divisible by 3 & 5

---
+++ spec/JazzbuzzSpec.js
@@ -0,0 +1,31 @@
+describe ('Jazzbuzz', function() { 
+
+	var jazzbuzz;
+
+	describe ('knows when a number is', function() { 
+
+		it('divisible by three', function() { 
+			jazzbuzz = new Jazzbuzz();  
+			expect(jazzbuzz.isDivisibleByThree(3)).toBe(true);
+		});
+
+		it ('divisible by five', function() {
+			jazzbuzz = new Jazzbuzz();
+			expect(jazzbuzz.isDivisibleByFive(5)).toBe(true);
+		});
+	});
+
+	
+	describe ('knows when a number is NOT', function() { 
+
+		it('divisible by three', function() { 
+			jazzbuzz = new Jazzbuzz();  
+			expect(jazzbuzz.isDivisibleByThree(2)).toBe(false);
+		});
+
+		it('divisible by five', function() {
+			jazzbuzz = new Jazzbuzz ();
+			expect (jazzbuzz.isDivisibleByFive(2)).toBe(false);
+		});
+	});
+}); 
---
+++ src/Jazzbuzz.js
@@ -0,0 +1,10 @@
+function Jazzbuzz() {};
+
+	Jazzbuzz.prototype.isDivisibleByThree = function(number) {
+		return (number % 3 === 0);
+	};
+
+	Jazzbuzz.prototype.isDivisibleByFive = function(number) {
+		return(number % 5 === 0);
+	};
+
