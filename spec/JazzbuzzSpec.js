dec09c1774c98fa963acecd2bce3649335e28085
RachelNolan <mail@rachelnolan.com>
2014-10-14 14:32:36 +0100

    Refactored both src and spec files

--- spec/JazzbuzzSpec.js
+++ spec/JazzbuzzSpec.js
@@ -2,21 +2,22 @@ describe ('Jazzbuzz', function() {
 
 	var jazzbuzz;
 
+	beforeEach(function() {
+		jazzbuzz = new Jazzbuzz
+	});
+
 	describe ('knows when a number is', function() { 
 
 		it('divisible by three', function() { 
-			jazzbuzz = new Jazzbuzz();  
-			expect(jazzbuzz.isDivisibleByThree(3)).toBe(true);
+			expect(jazzbuzz.isDivisibleBy(3, 3)).toBe(true);
 		});
 
 		it ('divisible by five', function() {
-			jazzbuzz = new Jazzbuzz();
-			expect(jazzbuzz.isDivisibleByFive(5)).toBe(true);
+			expect(jazzbuzz.isDivisibleBy(5, 5)).toBe(true);
 		});
 
 		it('divisible by fifteen', function() {
-			jazzbuzz = new Jazzbuzz();
-			expect(jazzbuzz.isDivisibleByFifteen(15)).toBe(true);
+			expect(jazzbuzz.isDivisibleBy(15, 15)).toBe(true);
 		});
 	});
 
@@ -24,18 +25,15 @@ describe ('Jazzbuzz', function() {
 	describe ('knows when a number is NOT', function() { 
 
 		it('divisible by three', function() { 
-			jazzbuzz = new Jazzbuzz();  
-			expect(jazzbuzz.isDivisibleByThree(2)).toBe(false);
+			expect(jazzbuzz.isDivisibleBy(2, 3)).toBe(false);
 		});
 
 		it('divisible by five', function() {
-			jazzbuzz = new Jazzbuzz ();
-			expect (jazzbuzz.isDivisibleByFive(2)).toBe(false);
+			expect (jazzbuzz.isDivisibleBy(2, 5)).toBe(false);
 		});
 
 		it('divisible by fifteen', function() {
-			jazzbuzz = new Jazzbuzz();
-			expect(jazzbuzz.isDivisibleByFifteen(1)).toBe(false);
+			expect(jazzbuzz.isDivisibleBy(1, 15)).toBe(false);
 		});
 	});
 }); 
--- src/Jazzbuzz.js
+++ src/Jazzbuzz.js
@@ -1,13 +1,17 @@
 function Jazzbuzz() {};
 
-	Jazzbuzz.prototype.isDivisibleByThree = function(number) {
-		return (number % 3 === 0);
-	};
+	// Jazzbuzz.prototype.isDivisibleByThree = function(number) {
+	// 	return (number % 3 === 0);
+	// };
 
-	Jazzbuzz.prototype.isDivisibleByFive = function(number) {
-		return(number % 5 === 0);
-	};
+	// Jazzbuzz.prototype.isDivisibleByFive = function(number) {
+	// 	return(number % 5 === 0);
+	// };
 
-	Jazzbuzz.prototype.isDivisibleByFifteen = function(number) {
-		return(number % 15 === 0);
-	}
+	// Jazzbuzz.prototype.isDivisibleByFifteen = function(number) {
+	// 	return(number % 15 === 0);
+	// }
+
+	Jazzbuzz.prototype.isDivisibleBy = function(number, divisor) {
+		return(number % divisor === 0)
+	};
