db89803895f249f0e9abd9d06fdc76b26c15e363
RachelNolan <mail@rachelnolan.com>
2014-10-15 07:30:02 +0100

    number divisible by 5

--- spec/JavaBuzzSpec.js
+++ spec/JavaBuzzSpec.js
@@ -2,18 +2,28 @@ describe('JavaBuzz Game', function(){
 
 	var javabuzz;
 
+	beforeEach(function() {
+		javabuzz = new JavaBuzz();
+	});
+
 	describe('should know when a number is', function() {
 
 		it('divisible by three', function() {
-			javabuzz = new JavaBuzz();
 			expect(javabuzz.isDivisibleByThree(3)).toEqual(true);
+		});
+
+		it('divisible by five', function() {
+			expect(javabuzz.isDivisibleByFive(5)).toEqual(true);
 		});
 	});
 
 	describe('should know when a number is NOT', function() {
 		it('divisible by three', function()  {
-			javabuzz = new JavaBuzz();
 			expect(javabuzz.isDivisibleByThree(1)).toEqual(false);
+		});
+
+		it('divisible by five', function() {
+			expect(javabuzz.isDivisibleByFive(1)).toEqual(false);
 		});
 	});
 
