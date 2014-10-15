1a76798ce69188fdf0dc58eabab4abdfaee0c25c
RachelNolan <mail@rachelnolan.com>
2014-10-15 07:24:53 +0100

    number divisible by 3

---
+++ spec/JavaBuzzSpec.js
@@ -0,0 +1,20 @@
+describe('JavaBuzz Game', function(){
+
+	var javabuzz;
+
+	describe('should know when a number is', function() {
+
+		it('divisible by three', function() {
+			javabuzz = new JavaBuzz();
+			expect(javabuzz.isDivisibleByThree(3)).toEqual(true);
+		});
+	});
+
+	describe('should know when a number is NOT', function() {
+		it('divisible by three', function()  {
+			javabuzz = new JavaBuzz();
+			expect(javabuzz.isDivisibleByThree(1)).toEqual(false);
+		});
+	});
+
+});
