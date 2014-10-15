b11daa569d0e2d02cb46192ec40e5445cfb62625
RachelNolan <mail@rachelnolan.com>
2014-10-15 22:15:15 +0100

    Knows when number is/is not divisible by three

---
+++ spec/JavaBuzzSpec.js
@@ -0,0 +1,18 @@
+describe('JavaBuzz game', function() {
+
+	beforeEach(function() {
+		javabuzz = new JavaBuzz
+	});
+
+	describe('should know when a number is', function() {
+		it('divisible by three', function() {
+			expect(javabuzz.isDivisibleByThree(3)).toEqual(true);
+		});
+	});
+
+	describe('should know when a number is NOT', function() {
+		it('divisible by three', function() {
+			expect(javabuzz.isDivisibleByThree(1)).toEqual(false);
+		});
+	});
+});
