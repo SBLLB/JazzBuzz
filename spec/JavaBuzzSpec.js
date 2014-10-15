705a3780ffc7ec86378994ea7518905bd10c2eec
RachelNolan <mail@rachelnolan.com>
2014-10-15 22:17:25 +0100

    Knows when number is/is not divisible by five

--- spec/JavaBuzzSpec.js
+++ spec/JavaBuzzSpec.js
@@ -8,11 +8,19 @@ describe('JavaBuzz game', function() {
 		it('divisible by three', function() {
 			expect(javabuzz.isDivisibleByThree(3)).toEqual(true);
 		});
+
+		it('divisible by five', function() {
+			expect(javabuzz.isDivisibleByFive(5)).toEqual(true);
+		});
 	});
 
 	describe('should know when a number is NOT', function() {
 		it('divisible by three', function() {
 			expect(javabuzz.isDivisibleByThree(1)).toEqual(false);
+		});
+
+		it('divisible by five', function() {
+			expect(javabuzz.isDivisibleByFive(1)).toEqual(false);
 		});
 	});
 });
--- src/JavaBuzz.js
+++ src/JavaBuzz.js
@@ -3,3 +3,7 @@ JavaBuzz = function() {};
 JavaBuzz.prototype.isDivisibleByThree = function(number) {
 	return (number % 3 === 0);
 };
+
+JavaBuzz.prototype.isDivisibleByFive = function(number) {
+	return(number % 5 === 0);
+};
