8feb4f48ccc08803f287ed22bf8c2c4b12b08db3
RachelNolan <mail@rachelnolan.com>
2014-10-16 07:30:18 +0100

    is/is not divisible by five

--- spec/JavaBuzzSpec.js
+++ spec/JavaBuzzSpec.js
@@ -1,18 +1,26 @@
 describe('JavaBuzz', function(){
 
-
+	beforeEach(function(){
+		javabuzz = new JavaBuzz;
+	});
 
 	describe('should know when a number is', function() {
 		it('divisible by three', function() {
-			javabuzz = new JavaBuzz();
 			expect(javabuzz.isDivisibleByThree(3)).toEqual(true);
+		});
+
+		it('divisible by five', function() {
+			expect(javabuzz.isDivisibleByFive(5)).toEqual(true);
 		});
 	});
 
 	describe('should know when a number is NOT', function() {
 		it('divisible by three', function() {
-			javabuzz = new JavaBuzz;
 			expect(javabuzz.isDivisibleByThree(1)).toEqual(false);
+		});
+
+		it('divisible by five', function() {
+			expect(javabuzz.isDivisibleByFive(1)).toEqual(false);
 		});
 	});
 });
