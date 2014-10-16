4f732a0547370f6eaab0040f4231f93bd69d1b2a
RachelNolan <mail@rachelnolan.com>
2014-10-16 07:35:59 +0100

    refactored isDivisibleBy function

--- spec/JavaBuzzSpec.js
+++ spec/JavaBuzzSpec.js
@@ -31,4 +31,10 @@ describe('JavaBuzz', function(){
 			expect(javabuzz.isDivisibleByFifteen(1)).toEqual(false);
 		});
 	});
+
+	xdescribe('shoudl print', function() {
+		it('Java instead of multiples of three', function() {
+			expect(javabuzz.play(3)).toEqual("Java")
+		});
+	});
 });
