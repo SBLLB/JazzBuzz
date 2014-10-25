5dcfab29938f4e059aaf1b6e80d39b0b810dba2f
RachelNolan <mail@rachelnolan.com>
2014-10-25 15:02:54 +0100

    Number is divisible by 15

--- spec/JavaBuzzSpec.js
+++ spec/JavaBuzzSpec.js
@@ -1,15 +1,22 @@
 describe('FizzBuzz', function(){
 
+	beforeEach (function() {
+		fizzbuzz = new Fizzbuzz;
+
+	});
+
 	describe('should know when a number is', function() {
 
 		it('divisible by three', function() {
-			fizzbuzz = new Fizzbuzz;
 			expect(fizzbuzz.isDivisibleByThree(3)).toEqual(true);
 		});
 
 		it('divisible by five', function() {
-			fizzbuzz = new Fizzbuzz;
 			expect(fizzbuzz.isDivisibleByFive(5)).toEqual(true);
+		});
+
+		it('divisible by fifteen', function() {
+			expect(fizzbuzz.isDivisibleByFifteen(15)).toEqual(true);
 		});
 	});
 });
