943f0c34a2b9832abd94147fe6ee6b8ab6706c7d
RachelNolan <mail@rachelnolan.com>
2014-10-27 21:37:33 +0000

    Knows when divisible by 3

---
+++ spec/JavaBuzzSpec.js
@@ -0,0 +1,11 @@
+describe('Fizzbuzz', function() {
+
+	describe('should know when a number is', function() {
+
+		it('divisible by three', function() {
+			fizzbuzz = new Fizzbuzz;
+			expect(fizzbuzz.isDivisibleByThree(3)).toEqual(true);
+		});
+	});
+
+});
---
+++ src/JavaBuzz.js
@@ -0,0 +1,5 @@
+function Fizzbuzz() {};
+
+Fizzbuzz.prototype.isDivisibleByThree = function(number) {
+	return (number % 3 === 0); 
+};
