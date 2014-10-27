1ddb2069277c90b81a9040bf6abfba6b72e7ed85
RachelNolan <mail@rachelnolan.com>
2014-10-27 21:41:58 +0000

    Knows when numbers are NOT divisible by 3, 5, 15

--- spec/JavaBuzzSpec.js
+++ spec/JavaBuzzSpec.js
@@ -1,21 +1,38 @@
 describe('Fizzbuzz', function() {
 
+	beforeEach(function() {
+		fizzbuzz = new Fizzbuzz;
+	});
+
 	describe('should know when a number is', function() {
 
 		it('divisible by three', function() {
-			fizzbuzz = new Fizzbuzz;
 			expect(fizzbuzz.isDivisibleByThree(3)).toEqual(true);
 		});
 
 
 		it('divisible by five', function() {
-			fizzbuzz = new Fizzbuzz;
 			expect(fizzbuzz.isDivisibleByFive(5)).toEqual(true);
 		});
 
 		it('divisible by fifteen', function() {
-			fizzbuzz = new Fizzbuzz;
 			expect(fizzbuzz.isDivisibleByFive(15)).toEqual(true);
+		});
+	});
+
+		describe('should know when a number is NOT', function() {
+
+		it('divisible by three', function() {
+			expect(fizzbuzz.isDivisibleByThree(1)).toEqual(false);
+		});
+
+
+		it('divisible by five', function() {
+			expect(fizzbuzz.isDivisibleByFive(1)).toEqual(false);
+		});
+
+		it('divisible by fifteen', function() {
+			expect(fizzbuzz.isDivisibleByFive(1)).toEqual(false);
 		});
 	});
 
