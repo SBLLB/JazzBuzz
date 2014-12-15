6923409554d5f52e06c6a016e2bc8d770853dfa5
RachelNolan <mail@rachelnolan.com>
2014-12-15 17:17:14 +0000

    The game knows when a number is divisible by 3, 5 or 15

--- spec/JavaBuzzSpec.js
+++ spec/JavaBuzzSpec.js
@@ -1,56 +1,35 @@
-describe('Fizzbuzz in Javascript', function() {
+describe('A game of Fizzbuzz should', function() {
 
 	beforeEach(function() {
-		fizzbuzz = new Fizzbuzz;
+		fizzbuzz = new Fizzbuzz
 	});
 
-	describe('should know when a number is divisible', function() {
-		it('by three', function() {
-			expect(fizzbuzz.isDivisibleByThree(3)).toEqual(true)
+	describe('know when a number is', function() {
+
+		it('divisible by three', function() {
+			expect(fizzbuzz.isDivisibleByThree(3)).toEqual(true);
 		});
 
-		it('by five', function() {
+		it('divisible by five', function() {
 			expect(fizzbuzz.isDivisibleByFive(5)).toEqual(true);
 		});
 
-		it('by fifteen', function() {
+		it('divisible by fifteen', function() {
 			expect(fizzbuzz.isDivisibleByFifteen(15)).toEqual(true);
 		});
-
 	});
 
-	describe('should know when a number is not', function() {
+	describe('know when a number is NOT', function() {
 		it('divisible by three', function() {
 			expect(fizzbuzz.isDivisibleByThree(1)).toEqual(false);
 		});
 
 		it('divisible by five', function() {
 			expect(fizzbuzz.isDivisibleByFive(1)).toEqual(false);
-		});
+		});	
 
 		it('divisible by fifteen', function() {
 			expect(fizzbuzz.isDivisibleByFifteen(1)).toEqual(false);
 		});
 	});
-
-	describe('should say', function() {
-		it('Fizz when a number is divisible by three', function() {
-			expect(fizzbuzz.play(3)).toEqual('Fizz');
-		});
-
-		it('Buzz when a number is divisible by five', function() {
-			expect(fizzbuzz.play(5)).toEqual('Buzz')
-		});
-
-		it('FizzBuzz when a number is divisible by three and five', function() {
-			expect(fizzbuzz.play(15)).toEqual('FizzBuzz');
-		});
-
-		it('the number when it is not divisible by three or five', function() {
-			expect(fizzbuzz.play(1)).toEqual(1);
-		});
-	});
-
-
-
 });
--- src/JavaBuzz.js
+++ src/JavaBuzz.js
@@ -1,24 +1,13 @@
-function Fizzbuzz () {};
+function Fizzbuzz() {};
 
 Fizzbuzz.prototype.isDivisibleByThree = function(number) {
-	return this._isDivisibleBy(number, 3);
+	return number % 3 === 0;
 };
 
 Fizzbuzz.prototype.isDivisibleByFive = function(number) {
-	return this._isDivisibleBy(number, 5);
+	return number % 5 === 0; 
 };
 
 Fizzbuzz.prototype.isDivisibleByFifteen = function(number) {
-	return this._isDivisibleBy(number, 15);
-};
-
-Fizzbuzz.prototype._isDivisibleBy = function(number, divisor) {
-	return (number % divisor === 0);
-};
-
-Fizzbuzz.prototype.play = function(number) {
-	if (this.isDivisibleByFifteen(number)) return 'FizzBuzz';
-	if (this.isDivisibleByThree(number)) return 'Fizz';
-	if (this.isDivisibleByFive(number)) return 'Buzz';
-	return number;
+	return number % 15 === 0;
 };
